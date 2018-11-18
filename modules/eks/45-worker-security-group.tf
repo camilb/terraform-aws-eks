134eaf2a6af09f9bbcb00eb0f94120f3ae81ebf0
Camil Blanaru <camil@edka.io>
2018-11-18 22:45:53 +0100

    Update security group

--- modules/eks/45-worker-security-group.tf
+++ modules/eks/45-worker-security-group.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "worker" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   tags = "${
